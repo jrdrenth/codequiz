8b19978dda243082d821cac09c21bf6fe7cf317c
jrdrenth <john.drenth@gmail.com>
2021-05-23 22:34:04 -0700

    Added questions for quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    title: "In JavaScript, what is a block of code called that is used to perform a specific task?",
+    choices: ["Variable", "Declaration", "String", "Function"],
+    answer: "Function"
+  },
+  {
+    title: "What is the format called that is used for storing and transporting data?",
+    choices: ["JSON", "HTML", "Syntax", "Font"],
+    answer: "JSON"
+  },
+  {
+    title: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+    choices: ["Repeater", "Debugger", "Clone", "Loop"],
+    answer: "Loop"
+  },
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+    answer: "<script>"
+  },
+  {
+    title: "What value will get logged from the following:\nlet i = 10;\nlet j = i++;\nconsole.log(j);",
+    choices: ["Error", "blank", "10", "11"],
+    answer: "11"
+  }
+];
